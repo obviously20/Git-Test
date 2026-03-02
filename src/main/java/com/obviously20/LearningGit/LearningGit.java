8f4c2a886061fa2f3d89bf3ee2488b3dd2e52047
obviously20 <1317418062@qq.com>
2026-03-02 17:56:47 +0800

    second commit

--- src/main/java/com/obviously20/LearningGit/LearningGit.java
+++ src/main/java/com/obviously20/LearningGit/LearningGit.java
@@ -3,6 +3,6 @@ package com.obviously20.LearningGit;
 public class LearningGit {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("hello git2");
     }
 }
